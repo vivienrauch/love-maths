827c76cd1c43052d6a818bf9886897b03f8fccd3
vivienrauch <vivienmirjam@gmail.com>
2023-01-12 13:25:40 +0000

    Add more code to divison game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -87,6 +89,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
