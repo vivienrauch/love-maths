fb169776424cff565c7f5845e13f14c94040627d
vivienrauch <vivienmirjam@gmail.com>
2023-01-12 13:09:57 +0000

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,3 +137,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 }
+
+function displayDivisionQuestion(operand1, operand2) {
+
+	document.getElementById("operand1").textContent = operand1*operand2;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
+}
